87f7ca791c0c47ec1bd7aaec742e55b6f422cc68
柯鹏 <pipipeng.ke@gmail.com>
2024-04-18 09:46:19 +0800

    Initial commit

---
+++ src/layouts/components/Footer/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="footer flx-center">
+    <a> 2022 © Three-ME By Three-ME Technology. </a>
+    <!-- <a href="https://github.com/HalseySpicy" target="_blank"> 2022 © Three-ME By Three-ME Technology. </a> -->
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
